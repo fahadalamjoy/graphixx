f64eaf082b4c5c1cae43c192652481f8504c2405
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-10-03 17:08:38 +0600

    all feedback done

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -9,7 +9,7 @@ import Linkedin from '../assets/linkedin.png'
 
 export default function Footer() {
   return (
-    <section className='bg-black h-full text-white '>
+    <section className='bg-black h-full text-white py-12'>
         <div className='container mx-auto px-4 flex flex-col   '>
             <h1 className='lg:text-4xl text-2xl text-center py-4'>Or you can contact us</h1>
             <div className='flex flex-col items-start mx-auto lg:py-6 py-3'>
@@ -23,16 +23,16 @@ export default function Footer() {
                 </div>
                 <div className='flex mt-2 py-1 items-center align-middle'>
                     <img className='w-5' src={whatsapp} alt='logo' />
-                    <p className='pl-4 text-xl'>+44 7879 740842</p>
+                    <p className='pl-4 text-xl'>+448 0023 35778</p>
                 </div>
             </div>
             <div className='py-2'>
                 <h1 className='lg:text-2xl text-xl text-center'>Find Us On</h1>
                 <div className='flex justify-center gap-4 py-4'>
-                    <img src={Twitter} alt='logo' />
-                    <img src={Facebook} alt='logo' />
-                    <img src={Insta} alt='logo' />
-                    <img src={Linkedin} alt='logo' />
+                    <a href='https://x.com/graphix_ltd?s=21' target='_blank'> <img src={Twitter} alt='logo' /></a>
+                    <a href='http://facebook.com/graphixlgs' target='_blank'> <img src={Facebook} alt='logo' /></a>
+                    <a href='http://instagram.com/graphix.uk/' target='_blank'> <img src={Insta} alt='logo' /></a>
+                    <a href='https://www.linkedin.com/company/graphix-ltd/' target='_blank' > <img src={Linkedin} alt='logo' /></a>
                 </div>
             </div>
         </div>
