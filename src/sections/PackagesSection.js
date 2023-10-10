8d1024ad518b3bdb5063c56c6f7b1eb38e922d68
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-10-10 16:48:23 +0600

    feedback by niloy vai

--- src/sections/PackagesSection.js
+++ src/sections/PackagesSection.js
@@ -14,41 +14,51 @@ export default function PackagesSection() {
       };
     
   return (
-    <section className='bg-white h-full flex flex-col justify-center py-12'>
+    <section className='bg-white h-full flex flex-col justify-center py-6'>
         <h1 className='lg:text-4xl text-3xl text-center text-black pt-6 font-bold'>Find the perfect plan that’s right for you</h1>
       <div className='container mx-auto px-4 '>
         <div className='flex lg:flex-row flex-col lg:gap-0 gap-8  justify-around py-12'>
-            <div className='bg-black lg:w-3/12 w-full text-white flex flex-col items-center p-16 text-center font-bold' style={rounded}>
+            <div className='hover:bg-white hover:border-2 hover:rounded-3xl hover:border-black lg:w-3/12 w-full text-black flex flex-col items-center p-16 text-center font-bold'>
                 <div><img src={Basic} alt='skilImage' />  </div>
-                <h1 className='pt-1'>Basic</h1>
-                <h1 className='text-2xl'>£89/mo</h1>
-                <div className='pt-4'>
+                <h1 className='pt-1 text-2xl font-bold'>Basic</h1>
+                <h1 className='text-4xl font-extrabold'>£89 <span className='text-base'> /mo</span></h1>
+                <div className='pt-2'>
                     <h1 className='text-2xl'>9 Contents /mo</h1>
                     {/* <h1>£20 for SEO Service</h1>
                     <h1 className='text-xs'>£10 for Social Media Posting</h1> */}
                 </div>
+                <div className='px-4 py-2 mt-4 bg-black text-white rounded-md'>
+                    <h1 className='text-xs'>Buy Now</h1>
+                </div>
             </div>
-            <div className='bg-black lg:w-3/12 w-full text-white flex flex-col items-center p-16 text-center font-bold' style={triangle}>
+            <div className='hover:bg-white hover:border-2 hover:rounded-3xl hover:border-black lg:w-3/12 w-full text-black flex flex-col items-center p-16 text-center font-bold'>
                 <div><img src={Standard} alt='skilImage' />  </div>
-                <h1 className='pt-1'>Standard</h1>
-                <h1 className='text-2xl'>£89/mo</h1>
-                <div className='pt-4'>
-                    <h1 className='text-2xl'>9 Contents /mo</h1>
+                <h1 className='pt-1 text-2xl font-bold'>Standard</h1>
+                <h1 className='text-4xl font-extrabold'>£169 <span className='text-base'> /mo</span></h1>
+                <div className='pt-2'>
+                    <h1 className='text-2xl'>18 Contents <span className='text-base'> /mo</span></h1>
                     {/* <h1>£20 for SEO Service</h1>
                     <h1 className='text-xs'>£10 for Social Media Posting</h1> */}
                 </div>
+                <div className='px-4 py-2 mt-4 bg-black text-white rounded-md'>
+                    <h1 className='text-xs'>Buy Now</h1>
+                </div>
             </div>
-            <div className='bg-black lg:w-3/12 w-full text-white flex flex-col items-center p-16 text-center font-bold'>
-                <div><img src={Pro} alt='skilImage' />  </div>
-                <h1 className='pt-1'>Pro</h1>
-                <h1 className='text-2xl'>£89/mo</h1>
-                <div className='pt-4'>
-                    <h1 className='text-2xl'>9 Contents /mo</h1>
+            <div className='hover:bg-white hover:border-2 hover:rounded-3xl hover:border-black lg:w-3/12 w-full text-black flex flex-col items-center p-16 text-center font-bold'>
+                <div ><img src={Pro} alt='skilImage' />  </div>
+                <h1 className='pt-1 text-2xl font-bold'>Pro</h1>
+                <h1 className='text-4xl font-extrabold'>£229 <span className='text-base'> /mo</span></h1>
+                <div className='pt-2'>
+                    <h1 className='text-2xl'>25 Contents <span className='text-base'>/mo</span></h1>
                     {/* <h1>£20 for SEO Service</h1>
                     <h1 className='text-xs'>£10 for Social Media Posting</h1> */}
+                </div>
+                <div className='px-4 py-2 mt-4 bg-black text-white rounded-md'>
+                    <h1 className='text-xs'>Buy Now</h1>
                 </div>
             </div>
         </div>
+                <h1 className='text-2xl text-center text-black'>SEO (Search Engine Optimisation) Add-On Service Available!</h1>
       </div>
 
     </section>
